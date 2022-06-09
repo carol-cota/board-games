d108d7965f66ce1c6e6a91af0134ad6b4ecc39bf
carol-cota <carolina.cota@n26.com>
2022-06-09 12:53:06 +0200

    Add POI library

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.apache.poi:poi:5.2.2")
+    implementation("org.apache.poi:poi-ooxml:5.2.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
