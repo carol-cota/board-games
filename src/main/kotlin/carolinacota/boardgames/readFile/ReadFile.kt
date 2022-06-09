3532c7075cc1b85ede4986bb5b8f159bb41b98da
carol-cota <carolina.cota@n26.com>
2022-06-09 14:06:07 +0200

    Parse name

--- src/main/kotlin/carolinacota/boardgames/readFile/ReadFile.kt
+++ src/main/kotlin/carolinacota/boardgames/readFile/ReadFile.kt
@@ -1,7 +1,6 @@
 package carolinacota.boardgames.readFile
 
-import org.apache.poi.ss.usermodel.Workbook
-import org.apache.poi.ss.usermodel.WorkbookFactory
+import org.apache.poi.xssf.usermodel.XSSFSheet
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.File
 import java.io.FileInputStream
@@ -10,16 +9,21 @@ import java.io.FileInputStream
 class ReadFile(
     val filePath: String,
 ) {
-    fun call() = List<BoardGame>(7) { parseFile(it) }
-
+    fun call() = List<BoardGame>(7) { parseFile(it+1) }
 
     private fun parseFile(row: Int) : BoardGame {
+        val sheet = getSheet()
+        val id = sheet.getRow(row).getCell(0).numericCellValue.toInt().toString()
+        val name = sheet.getRow(row).getCell(1).toString()
+        return BoardGame(id, name)
+    }
+
+    private fun getSheet() : XSSFSheet {
         val file = File(filePath)
         val inputStream = FileInputStream(file)
         val workBook = XSSFWorkbook(inputStream)
-        val sheet = workBook.getSheetAt(0)
-        val id = sheet.getRow(row+1).getCell(0).numericCellValue.toInt().toString()
-        val name = sheet.getRow(row + 1).getCell(1).toString()
-        return BoardGame(id, name)
+        return workBook.getSheetAt(0)
     }
+
+    private fun getRowsCount() = getSheet().lastRowNum
 }
