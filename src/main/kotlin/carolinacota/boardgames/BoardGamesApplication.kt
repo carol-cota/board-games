d6495ebd862d6f67490bf3773181b59bbafcc9b3
carol-cota <carolina.cota@n26.com>
2022-06-28 12:41:24 +0200

    Add ReadFile to application context

--- src/main/kotlin/carolinacota/boardgames/BoardGamesApplication.kt
+++ src/main/kotlin/carolinacota/boardgames/BoardGamesApplication.kt
@@ -1,11 +1,24 @@
 package carolinacota.boardgames
 
+import carolinacota.boardgames.readFile.ReadFile
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
 
 @SpringBootApplication
-class BoardGamesApplication
+class BoardGamesApplication : CommandLineRunner{
 
+    @Autowired
+    private lateinit var applicationContext: ApplicationContext
+
+    override fun run(vararg args: String?) {
+        val readFile = applicationContext.getBean(ReadFile::class.java)
+        readFile.call()
+    }
+
+}
 fun main(args: Array<String>) {
-    runApplication<BoardGamesApplication>(*args)
+        runApplication<BoardGamesApplication>(*args)
 }
