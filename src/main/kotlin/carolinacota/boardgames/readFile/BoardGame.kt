11576e9e91c0343e61eeae4892009dbca6674ab3
carol-cota <carolina.cota@n26.com>
2022-06-28 12:34:31 +0200

    remove non used properties

--- src/main/kotlin/carolinacota/boardgames/readFile/BoardGame.kt
+++ src/main/kotlin/carolinacota/boardgames/readFile/BoardGame.kt
@@ -3,16 +3,6 @@ package carolinacota.boardgames.readFile
 data class BoardGame(
     val id: String,
     val name: String,
-    val yearPublished: String? = null,
-    val minPlayers: Int? = null,
-    val maxPlayers: Int? = null,
-    val playTime: Int? = null,
-    val minAge: Int? = null,
     val numberOfRatings: Int? = null,
-    val ratingAverage: Double? = null,
-    val bggRank: Int? = null,
-    val complexityAverage: Double? = null,
-    val ownedGames: Int? = null,
-    val mechanics: String? = null,
-    val domains: String? = null,
+    val ratingAverage: Double? = null
 )
--- src/main/kotlin/carolinacota/boardgames/readFile/ReadFile.kt
+++ src/main/kotlin/carolinacota/boardgames/readFile/ReadFile.kt
@@ -3,45 +3,28 @@ package carolinacota.boardgames.readFile
 import org.apache.poi.xssf.usermodel.XSSFCell
 import org.apache.poi.xssf.usermodel.XSSFSheet
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
 import java.io.File
 import java.io.FileInputStream
 
-
+@Service
 class ReadFile(
+    @Value("\${boardGame.data}")
     private val filePath: String,
 ) {
-    fun call() = List<BoardGame>(rowsCount) { parseFile(it + 1) }
+    fun call() = List<BoardGame>(getSheet().lastRowNum) { parseFile(it + 1) }
 
     private fun parseFile(row: Int): BoardGame {
         val id = cell(row, 0).toIntCell().toString()
         val name = cell(row, 1).toString()
-        val yearPublished = cell(row, 2).toString()
-        val minPlayers = cell(row, 3).toIntCell()
-        val maxPlayers = cell(row, 4).toIntCell()
-        val playTime = cell(row, 5).toIntCell()
-        val minAge = cell(row, 6).toIntCell()
         val numberOfRatings = cell(row, 7).toIntCell()
         val ratingAverage = cell(row, 8).toDoubleCell()
-        val bggRank = cell(row, 9).toIntCell()
-        val complexityAverage = cell(row, 10).toDouble2DigitCell()
-        val ownedGames = cell(row, 11).toIntCell()
-        val mechanics = cell(row, 12).toString()
-        val domains = cell(row, 13).toString()
         return BoardGame(
             id,
             name,
-            yearPublished,
-            minPlayers,
-            maxPlayers,
-            playTime,
-            minAge,
             numberOfRatings,
-            ratingAverage,
-            bggRank,
-            complexityAverage,
-            ownedGames,
-            mechanics,
-            domains
+            ratingAverage
         )
     }
 
@@ -52,30 +35,9 @@ class ReadFile(
         return workBook.getSheetAt(0)
     }
 
-    private val rowsCount = getSheet().lastRowNum
-
     private fun cell(row: Int, num: Int) = getSheet().getRow(row).getCell(num)
 
     private fun XSSFCell.toIntCell() = numericCellValue.toInt()
 
     private fun XSSFCell.toDoubleCell() = numericCellValue
-
-    private fun XSSFCell.toDouble2DigitCell() = "%.2f".format(numericCellValue).toDouble()
-
-    private val titleMapping = mapOf(
-        "id" to 0,
-        "name" to 1,
-        "yearPublished" to 2,
-        "minPlayers" to 3,
-        "maxPlayers" to 4,
-        "playTime" to 5,
-        "minAge" to 6,
-        "numberOfRatings" to 7,
-        "ratingAverage" to 8,
-        "bggRank" to 9,
-        "complexityAverage" to 10,
-        "ownedGames" to 11,
-        "mechanics" to 12,
-        "domains" to 13
-    )
 }
