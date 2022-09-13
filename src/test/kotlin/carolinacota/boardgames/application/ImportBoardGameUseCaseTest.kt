03ede17088a3d296fcdb40e167a3a24056fd143f
carol-cota <carolina.cota@n26.com>
2022-09-13 12:05:25 +0200

    Add ImportBoardGameUseCaseTest

--- src/test/kotlin/carolinacota/boardgames/application/ImportBoardGameUseCaseTest.kt
+++ src/test/kotlin/carolinacota/boardgames/application/ImportBoardGameUseCaseTest.kt
@@ -3,31 +3,39 @@ package carolinacota.boardgames.application
 import carolinacota.boardgames.domain.BoardGame
 import carolinacota.boardgames.domain.BoardGameParser
 import carolinacota.boardgames.domain.Repository
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.given
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 
-internal class ImportBoardGameUseCaseTest{
+internal class ImportBoardGameUseCaseTest {
     private val boardGameRepository = mock<Repository<BoardGame>>()
     private val boardGameParser = mock<BoardGameParser>()
     private val importBoardGameUseCase = ImportBoardGameUseCase(boardGameRepository, boardGameParser)
 
     @Test
-    fun `should call the parser`() {
-//        when
+    fun `should call the repository with parser result`() {
+        val parserResult = listOf<BoardGame>(BoardGame("hello", "Hello", 2, 4.8))
+        given(boardGameParser.call()).willReturn(parserResult)
+
         importBoardGameUseCase.import()
-//        then
-        verify(boardGameParser).call()
+
+        verify(boardGameRepository).save(parserResult)
     }
 
     @Test
-    fun `should persist the parser result`() {
-//        given
-//        parser result
-//        when
-        importBoardGameUseCase.import()
-//        then
-        verify(boardGameParser).call()
-        verify(boardGameRepository).save()
+    fun `should forward boardGameParser exception`() {
+        given(boardGameParser.call()).willThrow(RuntimeException("Error"))
+
+        assertThatExceptionOfType(RuntimeException::class.java).isThrownBy { importBoardGameUseCase.import() }
+    }
+
+    @Test
+    fun `should forward boardGameRepository exception`() {
+        given(boardGameRepository.save(any())).willThrow(RuntimeException("Error"))
+
+        assertThatExceptionOfType(RuntimeException::class.java).isThrownBy { importBoardGameUseCase.import() }
     }
 }
